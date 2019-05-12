cd0d78cd24439bafa5f9200b000b08fd196f65a4
y-chan <yuto_tetuota@yahoo.co.jp>
2019-05-12 13:10:39 +0900

    fix genesis block data

--- networks.js
+++ networks.js
@@ -14,8 +14,8 @@ exports.livenet = {
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('039620E2CE08E0EEF19E1758CBF631978D6B004A67063BD0C210BDB6F3080000'),
-    merkle_root: hex('50F49401AF815CBC6EED7C3CB5BB0DD73B8E5EF6D1C05404F32A9A3C7E3D7E45'),
+    hash: hex('000008f3b6bd10c2d03b06674a006b8d9731f6cb58179ef1eee008cee2209603'),
+    merkle_root: hex('457e3d7e3c9a2af30454c0d1f65e8e3bd70dbbb53c7ced6ebc5c81af0194f450'),
     height: 0,
     nonce: 96906,
     version: 1,
